bfda4ef0a3f3646b5027dd99ddf9daaacee76109
qdeconinck <none@none>
2013-02-12 23:40:54 +0100

    Correction

--- src/be/ac/ucl/lfsab1509/llncampus/LoisirsTitle.java
+++ src/be/ac/ucl/lfsab1509/llncampus/LoisirsTitle.java
@@ -2,7 +2,7 @@ package be.ac.ucl.lfsab1509.llncampus;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+//import android.view.Menu;
 
 /**
  * Classe qui va gérer l'espace où on va lister les catégories de loisirs
