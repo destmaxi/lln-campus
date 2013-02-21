01f2d74e4e7d7da8cc2da5963f8b79e2672e2833
dmercier <none@none>
2013-02-21 15:33:34 +0100

    Modification de la partie d'appel à la base de donnée (en commentaire en attendant que la base de donnée soit débuggée)

--- src/be/ac/ucl/lfsab1509/llncampus/activity/AuditoriumActivity.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/AuditoriumActivity.java
@@ -1,10 +1,13 @@
 package be.ac.ucl.lfsab1509.llncampus.activity;
 
 
+import java.util.ArrayList;
+
 import be.ac.ucl.lfsab1509.llncampus.Database;
 import be.ac.ucl.lfsab1509.llncampus.LLNCampus;
 import be.ac.ucl.lfsab1509.llncampus.R;
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,30 +26,26 @@ public class AuditoriumActivity extends ListActivity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.auditorium);
 		
-		/*
-		LLNCampus llnCampus = new LLNCampus();
-		Database db=null;
-		if (llnCampus !=null){
-			db = llnCampus.getDatabase();
-		}
-		else
-		{
-			Toast.makeText(this, "Bordel", Toast.LENGTH_LONG).show();
-		}
+		/* FIXME : A reactiver une fois la db ok
+		Database DB = LLNCampus.getDatabase();
+		 
+		String[] cols = {"NAME"};
+		Cursor c = DB.select("poi", cols,"TYPE = 'auditoire'",null, null, null, null, null);
 		
-		if (db != null)
-		{
-			Toast.makeText(this, "Youpie! =D", Toast.LENGTH_LONG).show();
+		ArrayList<String> values = new ArrayList<String>();
+		while(c.moveToNext()){
+			values.add(c.getString(0));
 		}
-		else
-		{
-			Toast.makeText(this, "Fuck!", Toast.LENGTH_LONG).show();
-		}
+		c.close();
 		*/
-		ListView listView = (ListView) findViewById(android.R.id.list);
 		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
 		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
 		  "Linux", "OS/2" };
+		
+		
+		
+		ListView listView = (ListView) findViewById(android.R.id.list);
+		
 
 		// Define a new Adapter
 		// First parameter - Context
