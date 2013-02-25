2cdb0ed933d713bfcd6b2842c6b4ba5fcab4cccf
dmercier <none@none>
2013-02-25 12:35:23 +0100

    Modification guidage GPS vers auditoire : À pied par défaut.

--- src/be/ac/ucl/lfsab1509/llncampus/activity/DetailsAuditorium.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/DetailsAuditorium.java
@@ -6,8 +6,10 @@ import java.util.Locale;
 import be.ac.ucl.lfsab1509.llncampus.Auditorium;
 import be.ac.ucl.lfsab1509.llncampus.ExternalAppUtility;
 import be.ac.ucl.lfsab1509.llncampus.R;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -69,7 +71,12 @@ public class DetailsAuditorium extends LLNCampusActivity implements OnClickListe
 			Intent intent;
 			switch (v.getId()) {
 			case R.id.button_auditorium_gps:
-				ExternalAppUtility.openBrowser(DetailsAuditorium.this, "google.navigation:q="+auditorium.getLatitude()+","+auditorium.getLongitude());
+				intent = new Intent(android.content.Intent.ACTION_VIEW, 
+							Uri.parse("http://maps.google.com/maps?daddr="+auditorium.getLatitude()+","+auditorium.getLongitude()+ "&dirflg=w"));
+			    intent.setComponent(new ComponentName("com.google.android.apps.maps", 
+					            "com.google.android.maps.MapsActivity"));          
+				startActivity(intent);
+				//ExternalAppUtility.openBrowser(DetailsAuditorium.this, "google.navigation:dirflg=w&q="+auditorium.getLatitude()+","+auditorium.getLongitude());
 				break;
 			case R.id.button_subauditorium:
 				intent = new Intent(this, SubAuditoriumActivity.class);
