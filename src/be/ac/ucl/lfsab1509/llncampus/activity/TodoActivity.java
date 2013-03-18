ee345fe9a8b699360799db769ed1517a20c8cf37
bbaugnies <none@none>
2013-03-18 15:51:54 +0100

    Debut des categories de loisirs Pas encore utilisé, la main invoque LoisirTitle (nom pas en accord avec conventions) alors que les nouveautés sont dans LoisirsActivity.

---
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/TodoActivity.java
@@ -0,0 +1,16 @@
+package be.ac.ucl.lfsab1509.llncampus.activity;
+
+import be.ac.ucl.lfsab1509.llncampus.R;
+import android.os.Bundle;
+//import android.view.Menu;
+
+
+public class TodoActivity extends LLNCampusActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo);
+    }
+    
+}
--- src/be/ac/ucl/lfsab1509/llncampus/fragment/AuditoriumListFragment.java
+++ src/be/ac/ucl/lfsab1509/llncampus/fragment/AuditoriumListFragment.java
@@ -22,23 +22,7 @@ public class AuditoriumListFragment extends LLNCampusListFragment {
 	private IAuditorium auditorium;
 	private OnAuditoriumSelectedListener audSelectedListener;
 	
-	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		
-	    String content = values.get(position);
-	    
-	    if(content !=null)
-		{
-			/* TODO passer le int et pas le nom */
-			String nameAuditorium = content;
-			Log.d("NAME", nameAuditorium);
-			String[] cols = {"ID","NAME","LATITUDE", "LONGITUDE", "ADDRESS"};
-			Cursor c = super.db.select("Poi", cols, "NAME = "+ "'"+nameAuditorium+"'", null, null, null, null, null);
-			c.moveToFirst();
-			auditorium = new Auditorium(c.getInt(0), c.getString(1), c.getDouble(2), c.getDouble(3), c.getString(4));
-		}
-	    audSelectedListener.onAuditoriumSelected(auditorium);
-	}
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
@@ -54,21 +38,11 @@ public class AuditoriumListFragment extends LLNCampusListFragment {
 		c.close();
 		
 		
-		
-		//ListView listView = (ListView) findViewById(android.R.id.list);
-		
-
 		// Define a new Adapter
 		// First parameter - Context
 		// Second parameter - Layout for the row
 		// Third parameter - ID of the TextView to which the data is written
 		// Forth - the Array of data
-		
-
-		
-		
-        
-        
 
 		adapter=new ArrayAdapter<String>(
 	            this.getActivity(),android.R.layout.simple_list_item_1, values){
@@ -87,7 +61,6 @@ public class AuditoriumListFragment extends LLNCampusListFragment {
 	        }
 	    };
 	        //SET THE ADAPTER TO LISTVIEW
-	        setListAdapter(adapter);
 		
 		// Assign adapter to ListView
 		//listView.setAdapter(adapter); 
@@ -98,7 +71,6 @@ public class AuditoriumListFragment extends LLNCampusListFragment {
 			
 		});*/
 		
-
         setListAdapter(adapter);
         setHasOptionsMenu(true);
 	}
@@ -110,10 +82,30 @@ public class AuditoriumListFragment extends LLNCampusListFragment {
             audSelectedListener = (OnAuditoriumSelectedListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
-                    + " must implement OnTutSelectedListener");
+                    + " must implement OnAuditoriumSelectedListener");
         }
     }
 
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		
+	    String content = values.get(position);
+	    
+	    if(content !=null)
+		{
+			/* TODO passer le int et pas le nom */
+			String nameAuditorium = content;
+			Log.d("NAME", nameAuditorium);
+			String[] cols = {"ID","NAME","LATITUDE", "LONGITUDE", "ADDRESS"};
+			Cursor c = super.db.select("Poi", cols, "NAME = "+ "'"+nameAuditorium+"'", null, null, null, null, null);
+			c.moveToFirst();
+			auditorium = new Auditorium(c.getInt(0), c.getString(1), c.getDouble(2), c.getDouble(3), c.getString(4));
+		}
+	    audSelectedListener.onAuditoriumSelected(auditorium);
+	}
+	
+	
 	public interface OnAuditoriumSelectedListener {
 	    public void onAuditoriumSelected(IAuditorium aud);
 	}
