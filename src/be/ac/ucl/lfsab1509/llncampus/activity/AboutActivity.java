6a10931881aef82c154217fd987e7d829235f870
qdeconinck <none@none>
2013-03-25 00:19:19 +0100

    Version qui sera pushée sur le Master

--- src/be/ac/ucl/lfsab1509/llncampus/activity/AboutActivity.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/AboutActivity.java
@@ -1,18 +1,22 @@
 package be.ac.ucl.lfsab1509.llncampus.activity;
 
 import be.ac.ucl.lfsab1509.llncampus.R;
-import android.app.Activity;
 import android.os.Bundle;
 
 /*
 * This activity just shows an about message
  */
-public class AboutActivity extends Activity {
+public class AboutActivity extends LLNCampusActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.about);
 	}
+	
+	@Override  
+    protected void editActionBar(){
+    	//Nothing to do because it's a dialog!
+    }
 }
 
 
