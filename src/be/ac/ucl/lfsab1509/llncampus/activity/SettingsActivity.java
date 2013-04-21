33b44f9cc8595b606a7b8a1c740a66ae27de5ec3
qdeconinck <none@none>
2013-04-21 13:02:35 +0200

    Instauration de l'activité SettingsActivity (problème, depuis l'API 11, il semble qu'il vaut mieux faire nos options avec des fragments, ehhh oui...)
    --HG--
    branch : master

--- src/be/ac/ucl/lfsab1509/llncampus/activity/SettingsActivity.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/SettingsActivity.java
@@ -1,25 +1,37 @@
 package be.ac.ucl.lfsab1509.llncampus.activity;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import be.ac.ucl.lfsab1509.llncampus.R;
 
 /**
  * Edit settings
  * @author Damien
  */
-public class SettingsActivity extends LLNCampusActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        
+        // Make sure default values are applied.  In a real app, you would
+        // want this in a shared function that is used to retrieve the
+        // SharedPreferences wherever they are needed.
+        /* PreferenceManager.setDefaultValues(getActivity(),
+                R.xml.advanced_preferences, false);
+	*/
+        
+     // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs);
+        
+        super.onCreate(savedInstanceState);
+
+
+
         
         
     }
     
-    @Override  
-    protected void editActionBar(){
-    	//Nothing to do because it's a dialog!
-    }
+    
     
 }
