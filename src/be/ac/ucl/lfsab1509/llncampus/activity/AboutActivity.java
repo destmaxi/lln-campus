c7ef6854cf0110e778a18e96e454d9ca49b1dfdc
qdeconinck <none@none>
2013-05-05 01:09:21 +0200

    Version code dans le A propos

--- src/be/ac/ucl/lfsab1509/llncampus/activity/AboutActivity.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/AboutActivity.java
@@ -1,7 +1,11 @@
 package be.ac.ucl.lfsab1509.llncampus.activity;
 
 import be.ac.ucl.lfsab1509.llncampus.R;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * This activity just shows an about message
@@ -11,6 +15,18 @@ public class AboutActivity extends LLNCampusActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		TextView text = (TextView) findViewById(R.id.about_text);
+		String nameCode = "0";
+		try{
+			PackageManager manager = this.getPackageManager();
+			PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+			nameCode = info.versionName;
+		}
+		catch(NameNotFoundException nnf)
+		{
+			nnf.printStackTrace();
+		}
+		text.setText(getString(R.string.about_text) + nameCode);
 	}
 
 	@Override
