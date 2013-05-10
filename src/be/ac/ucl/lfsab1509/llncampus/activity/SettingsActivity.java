a3e11bf1c464d4a9b9b499da5cd6bc34d51020d5
qdeconinck <none@none>
2013-05-10 03:57:43 +0200

    Version prête pour le Play Store

--- src/be/ac/ucl/lfsab1509/llncampus/activity/SettingsActivity.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/SettingsActivity.java
@@ -36,13 +36,7 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Make sure default values are applied.  In a real app, you would
-        // want this in a shared function that is used to retrieve the
-        // SharedPreferences wherever they are needed.
-        /* PreferenceManager.setDefaultValues(getActivity(),
-                R.xml.advanced_preferences, false);
-	*/
+       
         
      // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.prefs);
