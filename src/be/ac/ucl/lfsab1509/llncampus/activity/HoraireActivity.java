b1960ef9ed75d64dfd6971b72f2a23a914e5282e
qdeconinck <none@none>
2014-01-28 12:15:57 +0100

    .
    --HG--
    branch : master

--- src/be/ac/ucl/lfsab1509/llncampus/activity/HoraireActivity.java
+++ src/be/ac/ucl/lfsab1509/llncampus/activity/HoraireActivity.java
@@ -11,7 +11,6 @@ import be.ac.ucl.lfsab1509.llncampus.activity.adapter.EventListAdapter;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.format.Time;
